4ffd8969f4e28754bf3828982f28944afbee8898
Juliusz Tluscik <juliusz.tluscik@gmail.com>
2023-04-18 00:02:34 +0200

    stan surowy

--- scripts/main.js
+++ scripts/main.js
@@ -18,14 +18,12 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 }); 
 
-
-
+//fadeIn
 $(document).ready(function() {
     $('.showElementOnStart').fadeIn(2300);
 });
 
 
-//fadeIn
 $(document).scroll(function() {
     $(".showElement:hidden").each(function() {
         var bottom_of_element = $(this).offset().top + $(this).outerHeight();
@@ -37,3 +35,54 @@ $(document).scroll(function() {
     });
 });
 
+
+// signature
+
+function makeSig(id) {
+    let temp = document.getElementById(id);
+    temp.style.strokeDashoffset = temp.getTotalLength()
+    temp.style.strokeDasharray = temp.getTotalLength()
+    temp.classList.add("sig")
+}
+
+function sigReset(id) {
+    let temp = document.getElementById(id);
+    temp.classList.remove("sig")
+}
+
+
+
+// load footer
+
+let isFooter = false;
+
+function loadFooter() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById("footer").innerHTML = this.responseText;
+        
+        $("#buttonFooter").click(_=>{            
+            const footer = $("#footer");            
+            if (isFooter) {
+                footer.css({top:"calc(100vh - 32px)"})
+
+                sigReset("sigJ");
+                sigReset("sigO");
+
+                isFooter = false;
+            }else{
+                footer.css({top:`calc(100vh - ${$("footer").height() + 32}px`})
+
+                makeSig("sigJ");
+                makeSig("sigO");
+
+                isFooter = true;
+            }
+        })
+
+    }
+    xhttp.open("GET", "/res/footer.html", true);
+    xhttp.send();
+}
+
+loadFooter()
